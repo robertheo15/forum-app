05650604e761c5305ec6e2beabc64f44f900a80c
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-05 14:34:05 +0700

    auth states

---
+++ src/states/auth/action.js
@@ -0,0 +1,51 @@
+import api from '../../utils/api';
+
+const ActionType = {
+  SET_AUTH_USER: 'SET_AUTH_USER',
+  UNSET_AUTH_USER: 'UNSET_AUTH_USER',
+};
+
+function setAuthUserActionCreator(authUser) {
+  return {
+    type: ActionType.SET_AUTH_USER,
+    payload: {
+      authUser,
+    },
+  };
+}
+
+function unsetAuthUserActionCreator() {
+  return {
+    type: ActionType.UNSET_AUTH_USER,
+    payload: {
+      authUser: null,
+    },
+  };
+}
+function asyncSetAuthUser({ email, password }) {
+  return async (dispatch) => {
+    try {
+      const { data } = await api.login({ email, password });
+      api.setAccessToken(data.token);
+      const authUser = await api.getUserLogged(data.token);
+      dispatch(setAuthUserActionCreator(authUser.data.data));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+function asyncUnsetAuthUser() {
+  return (dispatch) => {
+    api.setAccessToken('');
+    dispatch(unsetAuthUserActionCreator());
+  };
+}
+
+export {
+  ActionType,
+  setAuthUserActionCreator,
+  unsetAuthUserActionCreator,
+  asyncSetAuthUser,
+  asyncUnsetAuthUser,
+};
