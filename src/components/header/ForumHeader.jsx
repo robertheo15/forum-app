621606b27d8e36e32cc319f8889add43942c98f6
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-05 14:36:18 +0700

    Create ForumHeader.jsx

---
+++ src/components/header/ForumHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForumHeader = () => (
+  <header>
+    <div className="top-bar">
+      <h1>Dicoding Forum App</h1>
+    </div>
+  </header>
+);
+
+export default ForumHeader;
