0d6d99eb26b095ba4982ee9ba317902908bf2820
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-05 14:33:20 +0700

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import ForumBody from './components/body/ForumBody';
+import BottomNavigation from './components/navigation/BottomNavigation';
+import ForumHeader from './components/header/ForumHeader';
+import { asyncUnsetAuthUser } from './states/auth/action';
 
-function App() {
+import './styles/App.css';
+
+const App = () => {
+  const {
+    authUser = null,
+  } = useSelector((states) => states);
+
+  const dispatch = useDispatch();
+
+  const onSignOut = () => {
+    dispatch(asyncUnsetAuthUser());
+  };
+
+  // console.log(authUser);
+  // console.log(threads);
+  // console.log(users);
+  // console.log(threadList);
+
+  if (authUser === null || authUser === undefined) {
+    return (
+      <div className="app">
+        <ForumHeader />
+        <ForumBody />
+        <BottomNavigation />
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ForumHeader user={authUser} />
+      <ForumBody user={authUser} />
+      <BottomNavigation user={authUser} signOut={onSignOut} />
     </div>
   );
-}
+};
 
 export default App;
