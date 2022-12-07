a94dab4ed22e1b0e34b622cb0bc9144a82df7501
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-07 21:41:19 +0700

    Implement redux for leaderboards

---
+++ src/states/leaderoards/action.js
@@ -0,0 +1,17 @@
+const ActionType = {
+  RECEIVE_LEADERBOARDS: 'RECEIVE_LEADERBOARDS',
+};
+
+function receiveLeaderboardsActionCreator(leaderboards) {
+  return {
+    type: ActionType.RECEIVE_LEADERBOARDS,
+    payload: {
+      leaderboards,
+    },
+  };
+}
+
+export {
+  ActionType,
+  receiveLeaderboardsActionCreator,
+};
