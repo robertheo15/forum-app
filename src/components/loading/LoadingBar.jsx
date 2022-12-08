046fee228d863188fbb5ddf5a5312f99399d2fd3
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-09 00:24:03 +0700

    Create LoadingBar.jsx

---
+++ src/components/loading/LoadingBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoadingBar from 'react-redux-loading-bar';
+
+const Loading = () => (
+  <div className="loading">
+    <LoadingBar />
+  </div>
+);
+
+export default Loading;
