d72281c6337aa93ae6680e4eaf41aa77794ae89c
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-08 23:16:54 +0700

    Implement add comment

--- src/states/detailThread/action.js
+++ src/states/detailThread/action.js
@@ -2,9 +2,19 @@ import api from '../../utils/api';
 
 const ActionType = {
   RECEIVE_THREAD_DETAIL: 'RECEIVE_THREAD_DETAIL',
+  ADD_COMMMENT: 'ADD_COMMMENT',
   CLEAR_THREAD_DETAIL: 'CLEAR_THREAD_DETAIL',
   TOGGLE_LIKE_THREAD_DETAIL: 'TOGGLE_LIKE_THREAD_DETAIL',
 };
+
+function addCommentActionCreator(threadDetail) {
+  return {
+    type: ActionType.ADD_COMMMENT,
+    payload: {
+      threadDetail,
+    },
+  };
+}
 
 function receiveThreadDetailActionCreator(threadDetail) {
   return {
@@ -21,6 +31,18 @@ function clearThreadDetailActionCreator() {
   };
 }
 
+function asyncAddComment({ id, content }) {
+  return async (dispatch, getState) => {
+    const { threadDetail } = getState();
+    dispatch(addCommentActionCreator(threadDetail));
+
+    try {
+      await api.createComment(id, content);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
 function toggleLikeThreadDetailActionCreator(userId) {
   return {
     type: ActionType.TOGGLE_LIKE_THREAD_DETAIL,
@@ -58,6 +80,8 @@ function asyncToogleLikeThreadDetail() {
 export {
   ActionType,
   receiveThreadDetailActionCreator,
+  addCommentActionCreator,
+  asyncAddComment,
   clearThreadDetailActionCreator,
   toggleLikeThreadDetailActionCreator,
   asyncReceiveThreadDetail,
--- src/states/detailThread/reducer.js
+++ src/states/detailThread/reducer.js
@@ -4,6 +4,18 @@ function threadDetailReducer(threadDetail = null, action = {}) {
   switch (action.type) {
     case ActionType.RECEIVE_THREAD_DETAIL:
       return action.payload.threadDetail;
+    case ActionType.ADD_COMMMENT:
+      return threadDetail.comments.map((comment) => {
+        if (comment.id === action.payload.commentId) {
+          return {
+            ...comment,
+            comments: threadDetail.comments.includes(action.payload.comment)
+              ? threadDetail.comments.filter((id) => id !== action.payload.comment)
+              : threadDetail.comments.concat([action.payload.comment]),
+          };
+        }
+        return threadDetail;
+      });
     case ActionType.CLEAR_THREAD_DETAIL:
       return null;
     case ActionType.TOGGLE_LIKE_THREAD_DETAIL:
