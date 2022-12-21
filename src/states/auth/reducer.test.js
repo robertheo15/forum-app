542eb5b462c544e90c8240985a8d3c719e700167
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-22 01:25:35 +0700

    implement testing auth reducer

---
+++ src/states/auth/reducer.test.js
@@ -0,0 +1,71 @@
+import authUserReducer from './reducer';
+
+/**
+ * test scenario for authUserReducer
+ *
+ * - authUserReducer function
+ *  - should return the initial state when given by unknown action
+ *  - should return the authUser when given by authUser/set action
+ *  - should return the authUser when given by authUser/unset action
+ *
+ */
+
+
+describe('authUserReducer function', () => {
+  it('should return the initial state when given by unknown action', () => {
+    // arrange
+    const initialState = [];
+    const action = { type: 'UNKNOWN' };
+ 
+    // action
+    const nextState = authUserReducer(initialState, action);
+ 
+    // assert
+    expect(nextState).toEqual(initialState);
+ });
+
+ it('should return the authUser when given by authUser/set action', () => {
+   // arrange
+   const initialState = null;
+   const action = {
+     type: 'authUser/set',
+     payload: {
+      authUser: {
+        id: "john_doe",
+        name: "John Doe",
+        email: "john@example.com",
+        avatar: "https://generated-image-url.jpg"
+      }
+     },
+   };
+ 
+    // action
+    const nextState = authUserReducer(initialState, action);
+ 
+    // assert
+    expect(nextState).toEqual(action.payload.authUser);
+ });
+
+ it('should return null when given by authUser/unset action', () => {
+  // arrange
+  const initialState = {
+    id: "john_doe",
+    name: "John Doe",
+    email: "john@example.com",
+    avatar: "https://generated-image-url.jpg"
+  };
+
+  const action = {
+    type: 'authUser/unset',
+    payload: {
+      authUser: null
+    },
+  };
+
+   // action
+   const nextState = authUserReducer(initialState, action);
+
+   // assert
+   expect(nextState).toEqual(action.payload.authUser);
+  });
+});
