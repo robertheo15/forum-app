e2414ead1beaa0a12e2445e60a7254d5b25b8f3d
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-24 16:42:44 +0700

    Implement Like and Dislike feature

--- src/components/thread/ThreadItem.jsx
+++ src/components/thread/ThreadItem.jsx
@@ -6,17 +6,20 @@ import ThreadItemFooter from './ThreadItemFooter';
 import { userDTO } from '../../utils/dto';
 
 const ThreadItem = ({
-  id, body, title, category, user, createdAt, totalComments, upVotesBy, downVotesBy,
+  id, body, title, category, user, createdAt, totalComments, upVotesBy, downVotesBy, like, dislike,
 }) => (
   <div className="thread-item">
     <ThreadHeader id={id} title={title} category={category} />
     <div className="thread-item__body">{parse(body)}</div>
     <ThreadItemFooter
       createdAt={createdAt}
-      user={user}
+      user={(user === undefined) ? '' : user}
       totalComments={totalComments}
       upVotesBy={upVotesBy}
       downVotesBy={downVotesBy}
+      id={id}
+      like={like}
+      dislike={dislike}
     />
   </div>
 );
@@ -31,5 +34,7 @@ ThreadItem.propTypes = {
   downVotesBy: PropTypes.arrayOf(PropTypes.string),
   totalComments: PropTypes.number,
   user: PropTypes.shape(userDTO),
+  like: PropTypes.func,
+  dislike: PropTypes.func,
 };
 export default ThreadItem;
--- src/components/thread/ThreadList.jsx
+++ src/components/thread/ThreadList.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
 import ThreadItem from './ThreadItem';
 import { threadDTO } from '../../utils/dto';
+import { asyncToogleLikeThread, asyncToogleDislikeThread } from '../../states/threads/action';
 
-const ThreadList = ({ threadList }) => (
-  <div className="threads-list">
-    {
-      threadList.map((thread, index) => (
-        <ThreadItem key={`${index}`} {...thread} />
-      ))
-    }
-  </div>
-);
+const ThreadList = ({ threadList }) => {
+  const dispatch = useDispatch();
+
+  const onLike = ({ id }) => {
+    dispatch(asyncToogleLikeThread(id));
+  };
+
+  const onDislike = ({ id }) => {
+    dispatch(asyncToogleDislikeThread(id));
+  };
+
+  return (
+    <div className="threads-list">
+      {
+        threadList.map((thread, index) => (
+          <ThreadItem
+            key={`${index}`}
+            like={onLike}
+            dislike={onDislike}
+            {...thread}
+          />
+        ))
+      }
+    </div>
+  );
+};
 
 ThreadList.propTypes = {
   threadList: PropTypes.arrayOf(PropTypes.shape(threadDTO)).isRequired,
