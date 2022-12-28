29a63ccd4ef2a2caa28ad9b83effa50f501afe9c
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-28 21:35:34 +0700

    implement testing shared Thunk Function

---
+++ src/states/shared/action.test.js
@@ -0,0 +1,116 @@
+/**
+ * skenario test
+ *
+ * - asyncPopulateUsersAndThreads thunk
+ *  - should dispatch action correctly when data fetching success
+ *  - should dispatch action and call alert correctly when data fetching failed
+ */
+
+import { hideLoading, showLoading } from 'react-redux-loading-bar';
+import api from '../../utils/api';
+import { receiveThreadsActionCreator } from '../threads/action';
+import { receiveUsersActionCreator } from '../users/action';
+import { asyncPopulateUsersAndThreads } from './action';
+
+const fakeThreadsResponse = [
+  {
+    id: 'thread-1',
+    title: 'Thread Pertama',
+    body: 'Ini adalah thread pertama',
+    category: 'General',
+    createdAt: '2021-06-21T07:00:00.000Z',
+    ownerId: 'users-1',
+    upVotesBy: [],
+    downVotesBy: [],
+    totalComments: 0,
+  },
+  {
+    id: 'thread-2',
+    title: 'Thread Kedua',
+    body: 'Ini adalah thread kedua',
+    category: 'General',
+    createdAt: '2021-06-21T07:00:00.000Z',
+    ownerId: 'users-2',
+    upVotesBy: [],
+    downVotesBy: [],
+    totalComments: 0,
+  },
+];
+
+const fakeUsersResponse = [
+  {
+    id: 'john_doe',
+    name: 'John Doe',
+    email: 'john@example.com',
+    avatar: 'https://generated-image-url.jpg',
+  },
+  {
+    id: 'jane_doe',
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    avatar: 'https://generated-image-url.jpg',
+  },
+  {
+    id: 'fulan',
+    name: 'Si Fulan',
+    email: 'fulan@example.com',
+    avatar: 'https://generated-image-url.jpg',
+  },
+];
+
+const fakeErrorResponse = new Error('Ups, sometwhing went wrong');
+
+describe('asyncPopulateUsersAndThreads thunk', () => {
+  beforeEach(() => {
+    // backup original implementation
+    api._getUsers = api.getUsers;
+    api._getThreads = api.getThreads;
+  });
+
+  afterEach(() => {
+    // restore original implementation
+    api.getUsers = api._getUsers;
+    api.getThreads = api._getThreads;
+
+    // delete backup
+    delete api._getUsers;
+    delete api._getThreads;
+  });
+
+  it('should dispatch action correctly when data fetching success', async () => {
+    // arrange
+    // stub implementation
+    api.getUsers = () => Promise.resolve(fakeUsersResponse);
+    api.getThreads = () => Promise.resolve(fakeThreadsResponse);
+    // mock dispatch
+    const dispatch = jest.fn();
+
+    // action
+    await asyncPopulateUsersAndThreads()(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(receiveThreadsActionCreator(fakeThreadsResponse));
+    expect(dispatch).toHaveBeenCalledWith(receiveUsersActionCreator(fakeUsersResponse));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+  });
+
+  it('should dispatch action and call alert correctly when data fetching failed', async () => {
+    // arrange
+    // stub implementation
+    api.getUsers = () => Promise.reject(fakeErrorResponse);
+    api.getThreads = () => Promise.reject(fakeErrorResponse);
+    // mock dispatch
+    const dispatch = jest.fn();
+    // mock alert
+    window.alert = jest.fn();
+
+    // action
+    await asyncPopulateUsersAndThreads()(dispatch);
+
+    // assert
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(window.alert).toHaveBeenCalledWith(fakeErrorResponse.message);
+  });
+});
