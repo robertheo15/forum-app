f3882816b8be531d7cccec4b65cc0f1e5b9ff8f8
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-12-30 22:44:16 +0700

    Add React Strict Mode

--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import './styles/style.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
